f37781774561d3642adb50290d4d03f3d4477b95
yashpurwar1 <yashpurwar.yashpurwar.com>
2021-06-14 12:13:53 +0530

    Added Random to check attendance

---
+++ empCheck.sh
@@ -0,0 +1,11 @@
+#!/bin/bash -x
+
+echo "Welcome to employee wage management system"
+
+rand=$(($RANDOM%2))
+if [ $rand -eq 0 ]
+then
+	echo "Employee is absent"
+else
+	echo "Employee is present"
+fi
